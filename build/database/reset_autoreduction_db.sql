75a3f478fb1184049b9fc4d85eda777b78c8f67c
William Hayward <william.hayward@stfc.ac.uk>
2020-10-29 15:16:28 +0000

    Inserting temp password into 'build/database/reset_autoreduction_db.sql'.

--- build/database/reset_autoreduction_db.sql
+++ build/database/reset_autoreduction_db.sql
@@ -2,7 +2,7 @@
 -- Starting with MySQL 8 you no longer can (implicitly) create a user using the GRANT command. 
 -- Use CREATE USER instead, followed by the GRANT statement as below
 USE mysql;
-CREATE USER 'test-user'@'localhost' IDENTIFIED BY 'pass';
+CREATE USER 'test-user'@'localhost' IDENTIFIED BY 'Abcdefg!123';
 GRANT ALL ON autoreduction.* TO 'test-user'@'localhost';
 FLUSH PRIVILEGES;
 
